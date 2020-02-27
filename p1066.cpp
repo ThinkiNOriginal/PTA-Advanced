b1d58b0582439bed276b0db528f6bae37cc10af7
Lotus D <DingLQ@protonmail.com>
2020-02-27 16:05:56 +0800

    AVL tree; similar to p1123;

---
+++ p1066.cpp
@@ -0,0 +1,123 @@
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <queue>
+
+using namespace std;
+
+struct Node {
+	int val;
+	int height = 1;
+	Node *left = nullptr;
+	Node *right = nullptr;
+};
+
+
+
+int depth(Node* tree) {
+	if (tree) {
+		tree -> height = max(depth(tree -> left), depth(tree -> right)) + 1;
+		return tree -> height;
+	} else {
+		return 0;
+	}
+}
+
+
+Node* LL(Node* tree) {
+	auto p = tree -> left;
+	tree -> left = p -> right;
+	p -> right = tree;
+	return p;
+}
+
+Node* RR(Node* tree) {
+	auto p = tree -> right;
+	tree -> right = p -> left;
+	p -> left = tree;
+	return p;
+}
+
+Node* LR(Node* tree) {
+	auto p = tree -> left -> right;
+	tree -> left -> right = p -> left;
+	p -> left = tree -> left;
+	tree -> left = p;
+	return LL(tree);
+}
+
+Node* RL(Node* tree) {
+//	auto p = tree -> right -> left;
+//	tree -> right -> left = p -> right;
+//	p -> right = tree -> right;
+	auto p = tree -> right -> left;
+	tree -> right -> left = p -> right;
+	p -> right = tree -> right;
+	tree -> right  = p;
+	return RR(tree);
+	
+}
+
+
+
+void Balance(Node*& tree) {
+	if (tree) {
+		auto left_d = depth(tree -> left);
+		auto right_d = depth(tree -> right);
+		if (left_d - right_d == 2) {
+			if (depth(tree -> left -> left) > depth(tree -> left -> right))
+				tree = LL(tree);
+			else
+				tree = LR(tree);
+		
+		} else if (right_d - left_d == 2) {
+			if (depth(tree -> right -> right) > depth(tree -> right -> left))
+				tree = RR(tree);
+			else
+				tree = RL(tree);
+		}
+	}
+}
+		
+
+
+Node* buildAVL(Node* tree, int cur) {
+	if (tree) {
+		if (cur < tree -> val) {
+			tree -> left = buildAVL(tree -> left, cur);
+			Balance(tree -> left);
+			Balance(tree);
+		} else {
+			tree -> right = buildAVL(tree -> right, cur);
+			Balance(tree -> right);
+			Balance(tree);
+		}
+
+	} else {
+		tree = new Node;
+		tree -> val = cur;
+	}
+	return tree;
+}
+
+
+
+
+
+int main() {
+	int N;
+	scanf("%d", &N);
+	Node *tree = nullptr;
+	for (int i = 0; i < N; i++) {
+		int temp;
+		scanf("%d", &temp);
+		tree = buildAVL(tree, temp);
+	}
+
+	printf("%d", tree -> val);
+
+	return 0;
+}
+
+
+
